b2039c3666da14d094bb0f1c47041142775a263b
somoers <edsomosierra@gmail.com>
2024-01-20 19:12:10 +0800

    now

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/somoers/learnGo
+
+go 1.21.6
+
+require (
+	github.com/somoers/dog v0.0.0-20240120105639-fdddb79d4654 // indirect
+	github.com/somoers/puppy v0.0.0-20240120110415-a62fcbd4628a // indirect
+)
---
+++ learn-package/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/somoers/puppy"
+)
+
+func main() {
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+}
